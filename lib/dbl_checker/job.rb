88253de430491d0b64f1963e5683188a7da5b638
swiknaba <swiknaba@users.noreply.github.com>
2021-10-24 02:04:48 +0200

    fix accessing check options

--- lib/dbl_checker/job.rb
+++ lib/dbl_checker/job.rb
@@ -10,16 +10,17 @@ module DblChecker
     module ClassMethods
       def check_options(options = {})
         options.symbolize_keys!
-        DblChecker.configuration.default_check_options.merge(options)
+        @check_options = DblChecker.configuration.default_check_options.merge(options)
       end
     end
 
     module InstanceMethods
       def initialize
+        @check_options = self.class.instance_variable_get(:'@check_options')
         @check = DblChecker::Check.new(
           app_version: DblChecker.configuration.app_version,
-          name: self.class.check_options[:name],
-          description: self.class.check_options[:description],
+          name: @check_options[:name],
+          description: @check_options[:description],
           job_klass: self.class.name,
         )
         @errors = []
@@ -28,7 +29,7 @@ module DblChecker
       def perform_check(last_executed_at = nil)
         return unless due?(last_executed_at)
 
-        Timeout.timeout(self.class.check_options[:timeout_in_seconds]) do
+        Timeout.timeout(@check_options[:timeout_in_seconds]) do
           perform
         end
 
@@ -36,7 +37,7 @@ module DblChecker
         @errors << e.message
       rescue Timeout::Error => e
         @errors << e.message # "execution expired"
-        @check.timout_after_seconds = self.class.check_options[:timeout_in_seconds]
+        @check.timout_after_seconds = @check_options[:timeout_in_seconds]
       ensure
         # write from @errors here, so we collect any errors logged before an exception occurred
         @check.error = @errors.join('\n')
@@ -49,13 +50,13 @@ module DblChecker
       def due?(last_executed_at)
         return true if last_executed_at.nil? || last_executed_at.empty?
 
-        last_executed_at < self.class.check_options[:every].ago
+        last_executed_at < @check_options[:every].ago
       end
 
       def assert(success, message)
         return if success
 
-        if self.class.check_options[:aggregate_failures]
+        if @check_options[:aggregate_failures]
           @errors << message
         else
           raise DblChecker::AssertionFailed, message
