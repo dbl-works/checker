4ddf76775ff2fae0a02ab973b1e82c052967df05
swiknaba <swiknaba@users.noreply.github.com>
2021-10-30 17:59:23 +0200

    allow configuring mock adapters

--- lib/dbl_checker/configuration.rb
+++ lib/dbl_checker/configuration.rb
@@ -16,24 +16,27 @@ module DBLChecker
     attr_accessor :slack_webhook_url # optional
     attr_accessor :app_version # typically this is the commit hash of the current deployed code
     attr_accessor :default_check_options
-    attr_accessor :mock
-    attr_accessor :mock_job_executions
     attr_accessor :logger
+    attr_accessor :adapters
+    attr_accessor :dbl_checker_api_key # to persist events on DBL checker platform
 
     def initialize
       @logger = nil
-      @mock = false
-      @mock_job_executions = {}
       @slack_webhook_url = nil
+      @dbl_checker_api_key = nil
       @app_version = nil
       @default_check_options = {
-        every: 24.hours, # 24 hours
+        every: 24.hours,
         sla: 3.days,
         active: true,
         slack_channel: 'checkers',
         timeout_in_seconds: 30,
         aggregate_failures: false,
         runbook: nil,
+      }
+      @adapters = {
+        persistance: DBLChecker::Adapters::Persistance::DBLCheckerPlatform,
+        job_executions: DBLChecker::Adapters::JobExecutions::DBLCheckerPlatform,
       }
     end
   end
