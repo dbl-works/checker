20dab78257906098b4e12a43dee14b6b23ca0cf2
swiknaba <swiknaba@users.noreply.github.com>
2021-10-30 17:59:59 +0200

    cli options can parse config file path

--- lib/dbl_checker/manager/cli_args.rb
+++ lib/dbl_checker/manager/cli_args.rb
@@ -1,17 +1,19 @@
 #!/usr/bin/env ruby
 
 require 'slop'
-require_relative '../version'
+# require_relative '../version'
 
 # https://github.com/leejarvis/slop#usage
 opts = Slop.parse do |o|
-  o.bool '-mr', '--mock-remote', 'mock requests to DBL servers'
+  o.bool '-m', '--mock_remote', 'mock requests to DBL servers'
   o.string '-e', '--environment', 'sets the RAILS_ENV variable'
+  o.string '-c', '--config', 'path to the config file'
   o.on '--version', 'print the version' do
     puts DBLChecker::VERSION
     exit
   end
 end
 
-ENV['RAILS_ENV'] = opts[:environment] unless opts[:environment].empty?
-ENV['OPTS_MOCK'] = opts[:mock] unless opts[:mock].empty?
+ENV['RAILS_ENV'] = opts[:environment] if ENV['RAILS_ENV'].nil?
+ENV['DBL_CHECKER_MOCK_REMOTE'] = "#{@opts&.mock_remote?}"
+ENV['DBL_CHECKER_CONFIG_PATH'] = opts[:config]
