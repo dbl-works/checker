f054050b50747ecf7b84137ad05846028a5e2b50
swiknaba <swiknaba@users.noreply.github.com>
2021-12-22 11:20:54 +0100

    fix slack service

--- lib/dbl_checker/adapters/persistance/slack.rb
+++ lib/dbl_checker/adapters/persistance/slack.rb
@@ -2,24 +2,28 @@ require 'singleton'
 require 'faraday'
 
 module DBLChecker
-  class SlackNotifier
-    include Singleton
+  module Adapters
+    module Persistance
+      class Slack
+        include Singleton
 
-    def initialize
-      @webhook_url = DBLChecker.configuration.slack_webhook_url
-      @headers = { Accept: 'application/json', 'Content-Type': 'application/json' }
-    end
+        def initialize
+          @webhook_url = DBLChecker.configuration.slack_webhook_url
+          @headers = { Accept: 'application/json', 'Content-Type': 'application/json' }
+        end
 
-    def call(check)
-      raise DBLChecker::Errors::ConfigError, 'Must configure a Slack webhook URL' if @webhook_url.nil?
+        def call(check)
+          raise DBLChecker::Errors::ConfigError, 'Must configure a Slack webhook URL' if @webhook_url.nil?
 
-      Faraday.post(
-        @webhook_url,
-        check.to_json,
-        @headers,
-      )
-    rescue StandardError => e
-      raise DBLChecker::Errors::ServerError, "Failed to notify Slack. Error: #{e.message}"
+          Faraday.post(
+            @webhook_url,
+            check.to_json,
+            @headers,
+          )
+        rescue StandardError => e
+          raise DBLChecker::Errors::ServerError, "Failed to notify Slack. Error: #{e.message}"
+        end
+      end
     end
   end
 end
